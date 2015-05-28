567ab714fd1b4575f97549bb6b9af651125674cb
MendelDdS <mendelssohn.sa@ccc.ufcg.edu.br>
2015-05-28 11:00:01 -0300

    Adicionado front-end ao repositorio

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,7 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-
+    public static Result createAD() { return redirect(""); }
+    public static Result sobre() { return ok(sobre.render()); }
+    public static Result anuncios() { return ok(anuncios.render());}
 }
